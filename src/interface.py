1733f7d3130bc13642fe1b66a94e2606fef58751
meehdii18 <95209113+meehdii18@users.noreply.github.com>
2023-12-20 09:50:14 +0100

    interface test

---
+++ src/interface.py
@@ -0,0 +1,120 @@
+import customtkinter
+import os
+import platform
+from PIL import Image
+
+
+def select_frame_by_name(name):
+    # set button color for selected button
+    home_button.configure(fg_color=("gray75", "gray25") if name == "home" else "transparent")
+    frame_2_button.configure(fg_color=("gray75", "gray25") if name == "frame_2" else "transparent")
+    frame_3_button.configure(fg_color=("gray75", "gray25") if name == "frame_3" else "transparent")
+
+    # show selected frame
+    if name == "home":
+        home_frame.grid(row=0, column=1, sticky="nsew")
+    else:
+        home_frame.grid_forget()
+    if name == "frame_2":
+        second_frame.grid(row=0, column=1, sticky="nsew")
+    else:
+        second_frame.grid_forget()
+    if name == "frame_3":
+        third_frame.grid(row=0, column=1, sticky="nsew")
+    else:
+        third_frame.grid_forget()
+
+def home_button_event():
+    select_frame_by_name("home")
+
+def frame_2_button_event():
+    select_frame_by_name("frame_2")
+
+def frame_3_button_event():
+    select_frame_by_name("frame_3")
+
+def change_appearance_mode_event(new_appearance_mode):
+    customtkinter.set_appearance_mode(new_appearance_mode)
+
+app = customtkinter.CTk()
+app.title("SUPER CONNECT 4")
+app.geometry("1050x675")
+app.resizable(False,False)
+
+# Base theme
+customtkinter.set_appearance_mode("Dark")
+customtkinter.set_default_color_theme("dark-blue")  # Themes: "blue" (standard), "green", "dark-blue"
+if(platform.system() == "Windows"):
+    import pywinstyles
+    pywinstyles.apply_style(app, acrylic)
+
+# set grid layout 1x2
+app.grid_rowconfigure(0, weight=1)
+app.grid_columnconfigure(1, weight=1)
+
+# load images with light and dark mode image
+image_path = os.path.join(os.path.dirname(os.path.realpath(__file__)), "test_images")
+logo_image = customtkinter.CTkImage(Image.open(os.path.join(image_path, "CustomTkinter_logo_single.png")), size=(26, 26))
+large_test_image = customtkinter.CTkImage(Image.open(os.path.join(image_path, "large_test_image.png")), size=(500, 150))
+image_icon_image = customtkinter.CTkImage(Image.open(os.path.join(image_path, "image_icon_light.png")), size=(20, 20))
+home_image = customtkinter.CTkImage(light_image=Image.open(os.path.join(image_path, "home_dark.png")),
+                                    dark_image=Image.open(os.path.join(image_path, "home_light.png")), size=(20, 20))
+chat_image = customtkinter.CTkImage(light_image=Image.open(os.path.join(image_path, "chat_dark.png")),
+                                    dark_image=Image.open(os.path.join(image_path, "chat_light.png")), size=(20, 20))
+add_user_image = customtkinter.CTkImage(light_image=Image.open(os.path.join(image_path, "add_user_dark.png")),
+                                        dark_image=Image.open(os.path.join(image_path, "add_user_light.png")), size=(20, 20))
+
+# create navigation frame
+navigation_frame = customtkinter.CTkFrame(app, corner_radius=0)
+navigation_frame.grid(row=0, column=0, sticky="nsew")
+navigation_frame.grid_rowconfigure(4, weight=1)
+
+navigation_frame_label = customtkinter.CTkLabel(navigation_frame, text="  SUPER CONNECT 4", image=logo_image,
+                                                compound="left", font=customtkinter.CTkFont(size=15, weight="bold"))
+navigation_frame_label.grid(row=0, column=0, padx=20, pady=20)
+
+home_button = customtkinter.CTkButton(navigation_frame, corner_radius=0, height=40, border_spacing=10, text="MAIN MENU",
+                                      fg_color="transparent", text_color=("gray10", "gray90"), hover_color=("gray70", "gray30"),
+                                      image=home_image, anchor="w", command=home_button_event)
+home_button.grid(row=1, column=0, sticky="ew")
+
+frame_2_button = customtkinter.CTkButton(navigation_frame, corner_radius=0, height=40, border_spacing=10, text="GAME FRAME",
+                                         fg_color="transparent", text_color=("gray10", "gray90"), hover_color=("gray70", "gray30"),
+                                         image=chat_image, anchor="w", command=frame_2_button_event)
+frame_2_button.grid(row=2, column=0, sticky="ew")
+
+frame_3_button = customtkinter.CTkButton(navigation_frame, corner_radius=0, height=40, border_spacing=10, text="END MENU",
+                                         fg_color="transparent", text_color=("gray10", "gray90"), hover_color=("gray70", "gray30"),
+                                         image=add_user_image, anchor="w", command=frame_3_button_event)
+frame_3_button.grid(row=3, column=0, sticky="ew")
+
+appearance_mode_menu = customtkinter.CTkOptionMenu(navigation_frame, values=["Dark","Light", "System"],
+                                                   command=change_appearance_mode_event)
+appearance_mode_menu.grid(row=6, column=0, padx=20, pady=20, sticky="s")
+
+# Main Menu
+home_frame = customtkinter.CTkFrame(app, corner_radius=0, fg_color="transparent")
+home_frame.grid_columnconfigure(0, weight=1)
+
+home_frame_large_image_label = customtkinter.CTkLabel(home_frame, text="", image=large_test_image)
+home_frame_large_image_label.grid(row=0, column=0, padx=20, pady=10)
+
+home_frame_button_1 = customtkinter.CTkButton(home_frame, text="", image=image_icon_image)
+home_frame_button_1.grid(row=1, column=0, padx=20, pady=10)
+home_frame_button_2 = customtkinter.CTkButton(home_frame, text="CTkButton", image=image_icon_image, compound="right")
+home_frame_button_2.grid(row=2, column=0, padx=20, pady=10)
+home_frame_button_3 = customtkinter.CTkButton(home_frame, text="CTkButton", image=image_icon_image, compound="top")
+home_frame_button_3.grid(row=3, column=0, padx=20, pady=10)
+home_frame_button_4 = customtkinter.CTkButton(home_frame, text="CTkButton", image=image_icon_image, compound="bottom", anchor="w")
+home_frame_button_4.grid(row=4, column=0, padx=20, pady=10)
+
+# Game Frame
+second_frame = customtkinter.CTkFrame(app, corner_radius=0, fg_color="transparent")
+
+# End Menu
+third_frame = customtkinter.CTkFrame(app, corner_radius=0, fg_color="transparent")
+
+# select default frame
+select_frame_by_name("home")
+
+app.mainloop()
